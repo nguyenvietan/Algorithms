a738a69a0bc2536a7a984e192433e562741999f1
nguyenvietan <annv@coccoc.com>
2021-11-22 20:12:24 +0700

    delete a node in a BST

---
+++ leetcode/google/src/bst/DeleteANodeInBST.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * [Review] Delete a Node in a BST
+ * keys: recursion, delete a node in a tree (must keep the previous pointer by using recursion)
+*/
+class Solution {
+	private TreeNode findPredecessor(TreeNode root) {
+		if (root == null || root.left == null) return null;
+		TreeNode n = root.left;
+		while (n.right != null) n = n.right;
+		return n;
+	}
+
+	private TreeNode findSuccessor(TreeNode root) {
+		if (root == null || root.right == null) return null;
+		TreeNode n = root.right;
+		while (n.left != null) n = n.left;
+		return n;
+	}
+
+	public TreeNode deleteNode(TreeNode root, int key) {
+		// base cases
+		if (root == null) return null;	
+		// cases >, <, =, recursion
+		if (root.val > key) root.left = deleteNode(root.left, key);
+		else if (root.val < key) root.right = deleteNode(root.right, key);
+		else {
+			TreeNode p = findPredecessor(root);
+			TreeNode s = findSuccessor(root);
+			if (p == null && s == null) return null;
+			else if (p != null) { root.val = p.val; root.left = deleteNode(root.left, p.val); }
+			else { root.val = s.val; root.right = deleteNode(root.right, s.val); }		
+		}
+		// return
+		return root;						
+	}
+}
+
+
